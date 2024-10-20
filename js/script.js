ae0c2ac18ff57cf0358d8d7ffe9ae9973208599f
Matevarzma <mmilorava73@gmail.com>
2024-10-20 11:25:52 -0400

    Added some stuff

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var map = L.map('map').setView([51.505, -0.09], 13); // Coordinates for map center
+    var map = L.map('map').setView([40.7128, -74.0060], 12); // Coordinates for map center
   
     // Add tile layer from OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    
+
+    var marker = L.marker([40.7536, -73.9832]).addTo(map)
+    .bindPopup('Restroom')
+    .openPopup();
   
     // You can make API requests here, e.g., to fetch locations from your API
     fetch('https://api.example.com/get-map-data')
